82ed579293f39145ca5f8c78d0b8ba04aaf3fae3
missyaya <1187047388@qq.com>
2019-04-16 13:38:16 +0800

    222

--- main.js
+++ main.js
@@ -23,27 +23,29 @@ function createWindow () {
 
 
   // Create the browser window.
-  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
+  // const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
   win = new BrowserWindow({
-    width,
-    height,
+    width:526,
+    height:466,
+    maximizable:false,
     center:true,
-    minWidth:1366,
-    minHeight:768,
-    autoHideMenuBar:true
+    autoHideMenuBar:true,
+    resizable:false,
+    frame:false,
+    transparent: true
   })
 
 
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: '/app/index.html',
+    pathname: '/app/login.html',
     protocol: 'file:',
     slashes: true
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
