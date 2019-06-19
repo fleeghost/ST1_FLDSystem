02bfc733d623430511a669426ce4447b0f9f09ab
fleeghost <foreverlovejjjj@gmail.com>
2019-06-19 10:18:58 +0800

    222

--- assembly/IMHelper.js
+++ assembly/IMHelper.js
@@ -141,12 +141,6 @@
                                 , friend: friendInfo
                                 , group: []
                             }
-                            //获取群员接口（返回的数据格式见下文）
-                            , members: {
-                                url: '/t.html' //接口地址（返回的数据格式见下文）
-                                , type: 'get' //默认get，一般可不填
-                                , data: {} //额外参数
-                            }
                             //上传图片接口（返回的数据格式见下文），若不开启图片上传，剔除该项即可
                             , uploadImage: {
                                 url: '' //接口地址
@@ -157,26 +151,25 @@
                                 url: '' //接口地址
                                 , type: 'post' //默认post
                             }
-                            //扩展工具栏，下文会做进一步介绍（如果无需扩展，剔除该项即可）
-                            , tool: [{
-                                alias: 'code' //工具别名
-                                , title: '代码' //工具名称
-                                , icon: '&#xe64e;' //工具图标，参考图标文档
-                            }]
-                            , msgbox: layui.cache.dir + 'css/modules/layim/html/msgbox.html' //消息盒子页面地址，若不开启，剔除该项即可
-                            , find: layui.cache.dir + 'css/modules/layim/html/find.html' //发现页面地址，若不开启，剔除该项即可
                             , chatLog: layui.cache.dir + 'css/modules/layim/html/chatlog.html' //聊天记录页面地址，若不开启，剔除该项即可
                         });
                         //监听发送消息
                         layim.on('sendMessage', function(res){
                             let fromContextID = mine.contextIDs;
                             let userID = mine.id;
-                            let toContextIDs = friend.filter(item => item.id==res.to.id)[0].contextIDs;
                             let msg = res.mine.content;
-                            alert(fromContextID+'|'+userID+'|'+toContextIDs+'|'+msg);
-                            $.signalrApi.server.chatMsg(fromContextID,userID,toContextIDs,msg);
+                            $.signalrApi.server.chatMsg(fromContextID,userID,msg);
                         });
-
+                        layim.on('sign', function(value){
+                            post({
+                                path: '/Frame/SqlData'
+                            }, {
+                                    cmdname: 'UpdateUserSign',
+                                    userID: localStorage.getItem("UserID"),
+                                    sign:value
+                                }, (ret) => {
+                                });
+                          });  
 
 
                         setTimeout(() => {
